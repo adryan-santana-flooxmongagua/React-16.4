57f2939dec21fe522fd9b1bc8ad869d4bf10047d
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-04-11 08:59:20 -0300

    feat: create component with parameter

--- quests/src/index.js
+++ quests/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import First from './components/First';
+import GoodDay from './components/GoodDay';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<First />);
+root.render(<GoodDay nome="guilherme"/>);
